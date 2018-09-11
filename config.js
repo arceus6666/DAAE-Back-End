d271f16061a565186f0f70726e4b6faa689f628d
Daniel Mendoza <arceus6666@hotmail.com>
2018-09-11 14:57:12 -0400

    config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  port: process.env.PORT || 9000,
-  db: process.env.MONGODB_URI || 'mongodb:localhost27017/DAAE',
+  port: process.env.PORT || 7890,
+  db: process.env.MONGODB_URI || 'mongodb:localhost:27017/DAAE',
   SECRET_TOKEN: 'DAEE_token'
   // to check
 }
