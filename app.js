23816b1f480395b95f0bbeda3215e469333e5585
Daniel Mendoza <arceus6666@hotmail.com>
2018-11-12 21:04:11 -0400

    updates

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
+const userApi = require('../routes/user')
+
 /*
 const xxxApi = requiere('./routes/xxx')
 ...
@@ -9,8 +12,9 @@ const xxxApi = requiere('./routes/xxx')
 
 const app = express()
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
+app.use('/users', userApi)
 
 /*
 app.use('/xxxs', xxxApi)
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: process.env.PORT || 7890,
-  db: process.env.MONGODB_URI || 'mongodb:localhost:27017/DAAE',
+  db: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/DAAE',
   SECRET_TOKEN: 'DAEE_token'
   // to check
 }
