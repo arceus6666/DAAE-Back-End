3ba420758701eab6387435c062f8b31aeea2bfb9
Daniel Mendoza <arceus6666@hotmail.com>
2018-11-13 15:44:43 -0400

    last updates services pre beta

--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,8 @@ const ctrl = require('../controllers/user')
 const api = express.Router()
 
 api.post('/register', ctrl.register)
-api.get('/login', ctrl.login)
 api.put('/update', ctrl.update)
+api.get('/login', ctrl.login)
 api.get('/find', ctrl.getById)
 
 module.exports = api
